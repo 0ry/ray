e2b0b3d8f33ed5c2e8eb49aeb37fc404532b4da2
0ry <0ry.its@gmail.com>
2017-04-29 01:50:53 -0300

    Grow Vector3 module

--- src/vector.rs
+++ src/vector.rs
@@ -39,7 +39,72 @@ impl Vector3 {
         (self.x * other.x + self.y * other.y + self.z * other.z)
     }
 
-    
+    pub fn cross(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    // pub fn deserialize_normalized<D>(deserializer: D) -> Result<Vector3, D::Error>
+    //     where D: Deserializer
+    // {
+    //     let v3 = Vector3::deserialize(deserializer)?;
+    //     Ok(v3.normalize())
+    // }
+}
+
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, other: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Neg for Vector3 {
+    type Output = Vector3;
+
+    fn neg(self) -> Vector3 {
+        Vector3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, other: Vector3) -> Vector3 {
+        (self + other.neg())
+    }
+}
+
+impl Mul<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, other: f64) -> Vector3 {
+        Vector3 {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl Mul<Vector3> for f64 {
+    type Output = Vector3;
+
+    fn mul(self, other: Vector3) -> Vector3 {
+        (other * self)
+    }
 }
 
 #[cfg(test)]
@@ -70,4 +135,35 @@ fn dot_test() {
     };
 
     assert_eq!(v.dot(&v), v.norm());
+}
+
+//#[test]
+fn cross_test() {
+}
+
+//#[test]
+fn deserialize_normalized_test() {
+}
+
+#[test]
+fn sum_neg_sub_test() {
+    let zero = Vector3::zero();
+    let v = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+
+    assert_eq!((v - v).length(), zero.length());
+}
+
+#[test]
+fn mul_test() {
+    let v = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+
+    assert_eq!((2f64 * v).length(), v.length() * 2f64);
 }
